ed42a12b946c2984ef6364edb9b67255c18cd15e
Ida Naderian <ida.naderian@gmail.com>
2020-06-17 23:59:46 +0200

    Basic setup for the project.

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+export default function Index() {
+    return (
+        <Head>
+            <title>Test</title>
+            <link rel="stylesheet" href="https://use.typekit.net/izy8aoj.css" />
+        </Head>
+    );
+}
